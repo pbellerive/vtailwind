a5cd807e04fa57f401e3a8096f541ca4f607ee32
Karl Provencher <kprovencher2022@gmail.com>
2024-12-10 02:52:39 -0500

    changed to vite 6.0.3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
-module.exports = {
-    content: ['./src/**/*.js', './src/**/*.vue'],
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{vue,js,ts,jsx,tsx}",
+    ],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {}
