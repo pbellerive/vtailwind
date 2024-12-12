012e7e460cccdf5513087e30d7bd9f81430861a5
Karl Provencher <kprovencher2022@gmail.com>
2024-12-12 14:34:39 -0500

    test tailwindv4 update before changing to v4

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,9 @@ export default {
         "./index.html",
         "./src/**/*.{vue,js,ts,jsx,tsx}",
     ],
-    darkMode: false, // or 'media' or 'class'
+    darkMode: 'class',
     theme: {
         extend: {}
     },
-    variants: {
-        extend: {}
-    },
     plugins: []
-};
+}
